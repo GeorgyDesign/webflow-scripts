4f5a210fc4618e540709820e8b3b84b705834eb9
GeorgyBuns <68088300+GeorgyBuns@users.noreply.github.com>
2022-07-14 15:47:53 +0400

    Update

--- aaply/aaply-fullpage.js
+++ aaply/aaply-fullpage.js
@@ -8,92 +8,91 @@ let animationIsFinished = false;
 
 // fullpage.js initialization
 new fullpage("#fullpage", {
-  /* responsiveWidth: 766, */
-  scrollingSpeed: 700,
-  // Disabling “scrollBar: true” will allow using scrollable section
-  scrollBar: true,
-  /* verticalCentered: false */
-  fitToSection: false,
+	/* responsiveWidth: 766, */
+	scrollingSpeed: 700,
+	scrollBar: true, // Disabling “scrollBar: true” will allow using scrollable section
+	/* verticalCentered: false */
+	fitToSection: false,
 
-  // onLeave - метод, вызывающийся во время ПОКИДАНИЯ слайда
-  onLeave: function (origin, destination, direction, trigger) {
-    console.log("origin: " + origin.index); // узнаем в консоли текущий слайд
+	// onLeave - метод, вызывающийся во время ПОКИДАНИЯ слайда
+	onLeave: function (origin, destination, direction, trigger) {
+		console.log("origin: " + origin.index); // узнаем в консоли текущий слайд
 
-    let curTime = new Date().getTime();
+		let curTime = new Date().getTime();
 
-    var origin = this;
+		var origin = this;
 
-    //после покидания раздела 1
-    if (origin.index == 0 && direction == "down") {
-      console.log("s-1-down и s-1-down-test");
-      document.querySelector(".s-1-down").click();
-      /* document.querySelector('.s-1-down-test').click(); */
+		//после покидания раздела 1
+		if (origin.index == 0 && direction == "down") {
+			console.log("s-1-down и s-1-down-test");
+			document.querySelector(".s-1-down").click();
+			/* document.querySelector('.s-1-down-test').click(); */
 
-      clearTimeout(timeoutId);
+			clearTimeout(timeoutId);
 
-      timeoutId = setTimeout(function () {
-        scrollDelay = true;
-        fullpage_api.moveTo(destination.index + 1);
-        scrollDelay = false;
-      }, delayOneDown);
-      return scrollDelay;
-    }
+			timeoutId = setTimeout(function () {
+				scrollDelay = true;
+				fullpage_api.moveTo(destination.index + 1);
+				scrollDelay = false;
+			}, delayOneDown);
+			return scrollDelay;
+		}
 
-    // после покидания раздела 2
-    else if (origin.index == 1 && direction == "up") {
-      console.log("s-2-up");
-      document.querySelector(".s-2-up").click();
+		// после покидания раздела 2
+		else if (origin.index == 1 && direction == "up") {
+			console.log("s-2-up");
+			document.querySelector(".s-2-up").click();
 
-      timeoutId = setTimeout(function () {
-        scrollDelay = true;
-        fullpage_api.moveTo(destination.index + 1);
-        scrollDelay = false;
-      }, delayTwoUp);
-      return scrollDelay;
-    }
+			timeoutId = setTimeout(function () {
+				scrollDelay = true;
+				fullpage_api.moveTo(destination.index + 1);
+				scrollDelay = false;
+			}, delayTwoUp);
+			return scrollDelay;
+		}
 
-    // после покидания раздела 2
-    else if (origin.index == 1 && direction == "down") {
-      console.log("s-2-down");
-      document.querySelector(".s-2-down").click();
+		// после покидания раздела 2
+		else if (origin.index == 1 && direction == "down") {
+			console.log("s-2-down");
+			document.querySelector(".s-2-down").click();
 
-      /*timeoutId = setTimeout(function () {
+			/*timeoutId = setTimeout(function () {
         scrollDelay = true;
         fullpage_api.moveTo(destination.index + 1);
         scrollDelay = false;
       }, delay);
       return scrollDelay;*/
-    }
+		}
 
-    // после покидания раздела 3
-    else if (origin.index == 2 && direction == "up") {
-      console.log("s-3-up");
-      document.querySelector(".s-3-up").click();
+		// после покидания раздела 3
+		else if (origin.index == 2 && direction == "up") {
+			console.log("s-3-up");
+			document.querySelector(".s-3-up").click();
 
-      timeoutId = setTimeout(function () {
-        scrollDelay = true;
-        fullpage_api.moveTo(destination.index + 1);
-        scrollDelay = false;
-      }, delay);
-      return scrollDelay;
-    }
-  },
+			timeoutId = setTimeout(function () {
+				scrollDelay = true;
+				fullpage_api.moveTo(destination.index + 1);
+				scrollDelay = false;
+			}, delay);
+			return scrollDelay;
+		}
+	},
 
-  afterLoad: function (origin, destination, direction, trigger) {
-    console.log("origin: " + origin.index); // узнаем в консоли текущий слайд
+	afterLoad: function (origin, destination, direction, trigger) {
+		console.log("origin: " + origin.index); // узнаем в консоли текущий слайд
 
-    var origin = this;
+		var origin = this;
 
-    //открытие секции 2 с 1
-    if (origin.index == 0 && direction == "down") {
-      console.log("центр s-2-center");
-      document.querySelector(".s-2-center").click();
-    }
+		//открытие секции 2 с 1
+		if (origin.index == 0 && direction == "down") {
+			console.log("центр s-2-center");
+			document.querySelector(".s-2-center").click();
+		}
 
-    //открытие секции 2 с 1
-    /*if(origin.index == 1 && direction =='down'){
+		//открытие секции 2 с 1
+		/*if(origin.index == 1 && direction =='down'){
         console.log("центр s-3");
         document.querySelector('.s-2-down').click();
       }*/
-  }
+	}
 });
