f6a6a709b4396d46f3b10d105295d8331ecba4b6
Georgy Alaverdyan <68088300+GeorgyDesign@users.noreply.github.com>
2022-12-25 22:27:13 +0400

    Push

--- test/daniil-test.js
+++ test/daniil-test.js
@@ -9,20 +9,17 @@ const spline = new Application(canvas);
 spline
 	.load("https://prod.spline.design/5z4ncQDE79uF-4V1/scene.splinecode")
 	.then(() => {
+		// Get the current scroll position of the browser window
+		function getScrollPosition() {
+			return {
+				x: window.pageXOffset || document.documentElement.scrollLeft,
+				y: window.pageYOffset || document.documentElement.scrollTop
+			};
+		}
+
+		// Trigger the Spline scroll event when the user scrolls the browser window
 		window.addEventListener("scroll", () => {
+			const { x, y } = getScrollPosition();
 			spline.emitEvent("scroll");
 		});
 	});
-
-/*function getScrollPosition() {
-		return {
-			x: window.pageXOffset || document.documentElement.scrollLeft,
-			y: window.pageYOffset || document.documentElement.scrollTop
-		};
-	}
-	
-	// Update the position of the Spline scene based on the scroll position
-	function updateScenePosition() {
-		const { x, y } = getScrollPosition();
-		spline.setPosition(x, y);
-	} */
