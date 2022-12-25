87fd9762c6662fa5262fbb3160af47686f313812
Georgy Alaverdyan <68088300+GeorgyDesign@users.noreply.github.com>
2022-12-25 22:11:12 +0400

    Push

--- test/daniil-test.js
+++ test/daniil-test.js
@@ -2,14 +2,14 @@ import { Application } from "@splinetool/runtime";
 
 const canvas = document.getElementById("canvas3d");
 const spline = new Application(canvas);
-/*spline.load('https://prod.spline.design/5z4ncQDE79uF-4V1/scene.splinecode').then(() => {
+/* spline.load('https://prod.spline.design/5z4ncQDE79uF-4V1/scene.splinecode').then(() => {
 		spline.emitEvent('scroll', 'Mark (Scroll Event)');
 	}); */
 
-spline
+/* spline
 	.load("https://prod.spline.design/5z4ncQDE79uF-4V1/scene.splinecode")
 	.then(() => {
 		document.querySelector(".scroll").window.addEventListener("scroll", () => {
 			spline.emitEvent("scroll", "Mark (Scroll Event)");
 		});
-	});
+	}); */
