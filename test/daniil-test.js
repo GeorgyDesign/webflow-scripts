d325986f258594a66f95f665d0c044a725bc8adf
Georgy Alaverdyan <68088300+GeorgyDesign@users.noreply.github.com>
2022-12-25 19:58:52 +0400

    Update script

--- test/daniil-test.js
+++ test/daniil-test.js
@@ -3,3 +3,7 @@ import { Application } from "@splinetool/runtime";
 const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
 app.load("https://prod.spline.design/5z4ncQDE79uF-4V1/scene.splinecode");
+
+document.querySelector(".scroll").addEventListener("scroll", () => {
+	spline.emitEvent("scroll", "Mark (Scroll Event)");
+});
